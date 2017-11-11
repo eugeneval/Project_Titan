a81b84a68627a81378bedc8fffef5063fc0ef903
eugeneval <eugval@gmail.com>
2017-11-11 16:19:14 +0000

    Moved some functions to external modules

---
+++ Control/PX4.py
@@ -0,0 +1,20 @@
+from pymavlink import mavutil
+from dronekit import Command
+
+# For PX4 mode switching as it is currently unsuported in DroneKit
+def PX4setMode(vehicle, mavMode):
+    vehicle._master.mav.command_long_send(vehicle._master.target_system, vehicle._master.target_component,
+                                               mavutil.mavlink.MAV_CMD_DO_SET_MODE, 0,
+                                               mavMode,
+                                               0, 0, 0, 0, 0, 0)
+
+
+# Builds a command based on a LocationGlobal waypoint and a type (takeoff, waypoint, or land)
+def PX4Command(wp, type):
+    if type == "TO":
+        cmd = Command(0,0,0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0, 1, 0, 0, 0, 0, wp.lat, wp.lon, wp.alt)
+    if type == "WP":
+        cmd = Command(0,0,0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 1, 0, 0, 0, 0, wp.lat, wp.lon, wp.alt)
+    if type == "LND":
+        cmd = Command(0,0,0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_LAND, 0, 1, 0, 0, 0, 0, wp.lat, wp.lon, wp.alt)
+    return cmd
--- Control/main.py
+++ Control/main.py
@@ -1,69 +1,44 @@
 
 print("Starting")
-from dronekit import connect, VehicleMode, Command, LocationGlobal
-from pymavlink import mavutil
-import time, math
+from dronekit import connect
+# from pymavlink import mavutil
+from PX4 import PX4setMode, PX4Command
+from navigation import get_location_offset_meters
+import time
 
 # Settings
 connection_string = '127.0.0.1:14540'
 MAV_MODE_AUTO = 4
-
-# A custom function to for PX4 mode switching as it is currently unsuported in DroneKit
-def PX4setMode(mavMode):
-    vehicle._master.mav.command_long_send(vehicle._master.target_system, vehicle._master.target_component,
-                                               mavutil.mavlink.MAV_CMD_DO_SET_MODE, 0,
-                                               mavMode,
-                                               0, 0, 0, 0, 0, 0)
-
-def get_location_offset_meters(original_location, dNorth, dEast, alt):
-    """
-    Returns a LocationGlobal object containing the latitude/longitude `dNorth` and `dEast` metres from the
-    specified `original_location`. The returned Location adds the entered `alt` value to the altitude of the `original_location`.
-    The function is useful when you want to move the vehicle around specifying locations relative to
-    the current vehicle position.
-    The algorithm is relatively accurate over small distances (10m within 1km) except close to the poles.
-    For more information see:
-    http://gis.stackexchange.com/questions/2951/algorithm-for-offsetting-a-latitude-longitude-by-some-amount-of-meters
-    """
-    earth_radius=6378137.0 #Radius of "spherical" earth
-    #Coordinate offsets in radians
-    dLat = dNorth/earth_radius
-    dLon = dEast/(earth_radius*math.cos(math.pi*original_location.lat/180))
-
-    #New position in decimal degrees
-    newlat = original_location.lat + (dLat * 180/math.pi)
-    newlon = original_location.lon + (dLon * 180/math.pi)
-    return LocationGlobal(newlat, newlon,original_location.alt+alt)
 
 # Connect to vehicle.
 print("Connecting to vehicle on: %s" % (connection_string,))
 vehicle = connect(connection_string, wait_ready=True)
 vehicle.wait_ready('autopilot_version')
 
-# Get all vehicle attributes (state)
+# Get all vehicle attributes (state), uncomment as needed
 print("\nGet all vehicle attribute values:")
 print(" Autopilot Firmware version: %s" % vehicle.version)
-print("   Major version number: %s" % vehicle.version.major)
-print("   Minor version number: %s" % vehicle.version.minor)
-print("   Patch version number: %s" % vehicle.version.patch)
-print("   Release type: %s" % vehicle.version.release_type())
-print("   Release version: %s" % vehicle.version.release_version())
-print("   Stable release?: %s" % vehicle.version.is_stable())
-print(" Autopilot capabilities")
-print("   Supports MISSION_FLOAT message type: %s" % vehicle.capabilities.mission_float)
-print("   Supports PARAM_FLOAT message type: %s" % vehicle.capabilities.param_float)
-print("   Supports MISSION_INT message type: %s" % vehicle.capabilities.mission_int)
-print("   Supports COMMAND_INT message type: %s" % vehicle.capabilities.command_int)
-print("   Supports PARAM_UNION message type: %s" % vehicle.capabilities.param_union)
-print("   Supports ftp for file transfers: %s" % vehicle.capabilities.ftp)
-print("   Supports commanding attitude offboard: %s" % vehicle.capabilities.set_attitude_target)
-print("   Supports commanding position and velocity targets in local NED frame: %s" % vehicle.capabilities.set_attitude_target_local_ned)
-print("   Supports set position + velocity targets in global scaled integers: %s" % vehicle.capabilities.set_altitude_target_global_int)
-print("   Supports terrain protocol / data handling: %s" % vehicle.capabilities.terrain)
-print("   Supports direct actuator control: %s" % vehicle.capabilities.set_actuator_target)
-print("   Supports the flight termination command: %s" % vehicle.capabilities.flight_termination)
-print("   Supports mission_float message type: %s" % vehicle.capabilities.mission_float)
-print("   Supports onboard compass calibration: %s" % vehicle.capabilities.compass_calibration)
+# print("   Major version number: %s" % vehicle.version.major)
+# print("   Minor version number: %s" % vehicle.version.minor)
+# print("   Patch version number: %s" % vehicle.version.patch)
+# print("   Release type: %s" % vehicle.version.release_type())
+# print("   Release version: %s" % vehicle.version.release_version())
+# print("   Stable release?: %s" % vehicle.version.is_stable())
+# print(" Autopilot capabilities")
+# print("   Supports MISSION_FLOAT message type: %s" % vehicle.capabilities.mission_float)
+# print("   Supports PARAM_FLOAT message type: %s" % vehicle.capabilities.param_float)
+# print("   Supports MISSION_INT message type: %s" % vehicle.capabilities.mission_int)
+# print("   Supports COMMAND_INT message type: %s" % vehicle.capabilities.command_int)
+# print("   Supports PARAM_UNION message type: %s" % vehicle.capabilities.param_union)
+# print("   Supports ftp for file transfers: %s" % vehicle.capabilities.ftp)
+# print("   Supports commanding attitude offboard: %s" % vehicle.capabilities.set_attitude_target)
+# print("   Supports commanding position and velocity targets in local NED frame: %s" % vehicle.capabilities.set_attitude_target_local_ned)
+# print("   Supports set position + velocity targets in global scaled integers: %s" % vehicle.capabilities.set_altitude_target_global_int)
+# print("   Supports terrain protocol / data handling: %s" % vehicle.capabilities.terrain)
+# print("   Supports direct actuator control: %s" % vehicle.capabilities.set_actuator_target)
+# print("   Supports the flight termination command: %s" % vehicle.capabilities.flight_termination)
+# print("   Supports mission_float message type: %s" % vehicle.capabilities.mission_float)
+# print("   Supports onboard compass calibration: %s" % vehicle.capabilities.compass_calibration)
 print(" Global Location: %s" % vehicle.location.global_frame)
 print(" Global Location (relative altitude): %s" % vehicle.location.global_relative_frame)
 print(" Local Location: %s" % vehicle.location.local_frame)
@@ -97,7 +72,7 @@ while not home_position_set:
 print(" Home location: %s" % vehicle.home_location)
 
 # Set to Auto mode
-PX4setMode(MAV_MODE_AUTO)
+PX4setMode(vehicle, MAV_MODE_AUTO)
 time.sleep(1)
 print("Vehicle mode should be AUTO: %s" % vehicle.mode.name)
 
@@ -109,33 +84,27 @@ home = vehicle.location.global_relative_frame
 
 # takeoff to 10 meters
 wp = get_location_offset_meters(home, 0, 0, 10);
-cmd = Command(0,0,0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0, 1, 0, 0, 0, 0, wp.lat, wp.lon, wp.alt)
-cmds.add(cmd)
+cmds.add(PX4Command(wp, "TO"))
 
 # move 10 meters north
 wp = get_location_offset_meters(wp, 10, 0, 0);
-cmd = Command(0,0,0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 1, 0, 0, 0, 0, wp.lat, wp.lon, wp.alt)
-cmds.add(cmd)
+cmds.add(PX4Command(wp, "WP"))
 
 # move 10 meters east
 wp = get_location_offset_meters(wp, 0, 10, 0);
-cmd = Command(0,0,0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 1, 0, 0, 0, 0, wp.lat, wp.lon, wp.alt)
-cmds.add(cmd)
+cmds.add(PX4Command(wp, "WP"))
 
 # move 10 meters south
 wp = get_location_offset_meters(wp, -10, 0, 0);
-cmd = Command(0,0,0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 1, 0, 0, 0, 0, wp.lat, wp.lon, wp.alt)
-cmds.add(cmd)
+cmds.add(PX4Command(wp, "WP"))
 
 # move 10 meters west
 wp = get_location_offset_meters(wp, 0, -10, 0);
-cmd = Command(0,0,0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 1, 0, 0, 0, 0, wp.lat, wp.lon, wp.alt)
-cmds.add(cmd)
+cmds.add(PX4Command(wp, "WP"))
 
 # land
 wp = get_location_offset_meters(home, 0, 0, 10);
-cmd = Command(0,0,0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_LAND, 0, 1, 0, 0, 0, 0, wp.lat, wp.lon, wp.alt)
-cmds.add(cmd)
+cmds.add(PX4Command(wp, "LND"))
 
 # Upload mission
 cmds.upload()
